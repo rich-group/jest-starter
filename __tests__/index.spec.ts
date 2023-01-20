c422c7c6917f6ce326b0a52462cb377c5e379ae2
caiyong <yong.cai@kapeixi.com>
2023-01-20 14:45:41 +0800

    first commit

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,8 @@
+
+import log from '../lib/index'
+
+describe('test the log', () => {
+    test('log', () => {
+        expect(log()).toBeUndefined();
+    })
+}) 
---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+
+export default function log (): void {
+  console.log(1)
+  console.log(2)
+  console.log(3)
+  console.log(4)
+  console.log(5)
+  console.log(6)
+}
