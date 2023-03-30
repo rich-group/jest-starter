402c78344e3b71a5e99e5d1ce3ad1ac3dba84a93
caiyong <yong.cai@kapeixi.com>
2023-03-30 10:54:11 +0800

    fix(template): 添加template

---
+++ meta.js
@@ -0,0 +1,98 @@
+const path = require('path')
+
+const {
+  sortDependencies,
+  installDependencies,
+  runLintFix,
+  printMessage,
+} = require('./utils')
+const pkg = require('./package.json')
+
+const templateVersion = pkg.version
+
+module.exports = {
+  helpers: {
+    if_or(v1, v2, options) {
+
+      if (v1 || v2) {
+        return options.fn(this)
+      }
+
+      return options.inverse(this)
+    },
+    template_version() {
+      return templateVersion
+    },
+  },
+  prompts: {
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Project name',
+    },
+    description: {
+      type: 'string',
+      required: false,
+      message: 'Project description',
+      default: 'A jest unit project',
+    },
+    author: {
+      type: 'string',
+      message: 'Author',
+    },
+    autoInstall: {
+      type: 'list',
+      message:
+        'Should we run `install` for you after the project has been created? (recommended)',
+      choices: [
+        {
+          name: 'Yes, use Pnpm',
+          value: 'pnpm',
+          short: 'pnpm',
+        },
+        {
+          name: 'Yes, use NPM',
+          value: 'npm',
+          short: 'npm',
+        },
+        {
+          name: 'Yes, use Yarn',
+          value: 'yarn',
+          short: 'yarn',
+        },
+        {
+          name: 'No, I will handle that myself',
+          value: false,
+          short: 'no',
+        },
+      ],
+    },
+  },
+  filters: {
+    'pnpm-lock.yaml': 'autoInstall === "pnpm"',
+    'package-lock.json': 'autoInstall === "npm"',
+    'yarn.lock': 'autoInstall === "yarn"'
+  },
+  complete: function(data, { chalk }) {
+    const green = chalk.green
+    console.log(data)
+    sortDependencies(data, green)
+
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+    if (data.autoInstall) {
+      installDependencies(cwd, data.autoInstall, green)
+        .then(() => {
+          return runLintFix(cwd, data, green)
+        })
+        .then(() => {
+          printMessage(data, green)
+        })
+        .catch(e => {
+          console.log(chalk.red('Error:'), e)
+        })
+    } else {
+      printMessage(data, chalk)
+    }
+  },
+}
